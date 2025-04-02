94e2025ec07116dc01b892a27d5fc9ab7ff62b0a
Aditya <aadichauhan2356@gmail.com>
2025-04-02 11:23:33 +0530

    Day 2

---
+++ Day 2/day 2.js
@@ -0,0 +1,25 @@
+let x= 10
+let y=8
+// console.log(x&y)
+////////////////// ternaray operators /////////////
+
+//  var result = (x<y)?"X is greater":"x is small";
+//  console.log(result);
+
+//  (1==1)?console.log("Okay"):console.log("Not okay");
+ 
+//  var num = Number(prompt("Enter number"));
+//  (num%2==0)?console.log(num, " is Even"):console.log(num, " is odd");
+
+
+
+///////// object /////////
+
+const student = 
+{
+    Name:"Aditya",
+    age:34,
+    class:"SAP"
+};
+
+console.log(student);
