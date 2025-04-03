c4de5097f0f24ba0f60a93862910b94132e4d4b3
Aditya <aadichauhan2356@gmail.com>
2025-04-03 11:41:24 +0530

    day 3

---
+++ DAY 3/Day 3.js
@@ -0,0 +1,63 @@
+/////////////////////////// looping statements /////////////////////////////////
+
+// 1: For loop
+// 2: while loop
+// 3: do while loop
+
+// for loop can be classified into 3 types
+// a: for each
+// b: for of
+// c: for in
+
+// common for all loops 
+// initialization-->condition-->updation
+
+
+
+// for loop
+
+// for (initilization;condition;updation){
+// statements;
+// };
+
+// example:
+
+// for(let i=100;i>0;--i)
+// {
+//     if (i%2!=0){
+//         document.writeln(i);
+//     }
+// }
+
+// for each 
+
+
+
+// for of
+
+
+
+// for in
+
+
+
+// while
+
+let x=1
+while(x<=10){
+    
+    document.writeln("2 x "+x+"="+(x*2)+"<br>");
+
+    x++;
+};
+document.writeln("<br>")
+
+// do while
+let y=1
+do {
+    document.writeln("5 x "+y+"="+(y*5)+"<br>");
+    y++;
+} while (y<=10);
+
+
+
