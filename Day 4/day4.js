5af994a1d29c60cca42fec00bccd4f3d8f1bfed5
Aditya <aadichauhan2356@gmail.com>
2025-04-04 11:42:27 +0530

    day 4

---
+++ Day 4/day4.js
@@ -0,0 +1,12 @@
+/////////////////////// DOM (DOCUMENT OBJECT MODEL) ////////////////////////////
+
+// document is object
+// using dom to perform operations on the html page
+// i.e. we can edit the html page using javascript with help of dom
+
+//  ways to modify the html page using dom
+// 1. document.getElementById("id") - get element by id
+// 2. document.getElementsBytagName("tagname") - get element by tag name
+// 3. document.getElementsByClassName("classname") - get element by class name
+// 4. document.querySelector("selector") - get element by selector
+// 5. document.querySlectorAll("selector") - get all elements by selector
