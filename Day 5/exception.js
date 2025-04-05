5e69648efc5a10878779e1ef94327a05595b0d0d
Aditya <aadichauhan2356@gmail.com>
2025-04-05 16:17:31 +0530

    Day5 concluded

--- Day 5/exception.js
+++ Day 5/exception.js
@@ -7,8 +7,23 @@
 // 2: Runtime
 // 3: Logical
 
-try {
-    document.writeln("this is suppose to be error"+abc);
-} catch (error) {
-    alert(`An error occured: ${error.message}`);
-}
+
+// try {
+//     document.writeln("this is suppose to be error"+abc);
+// } catch (error) {
+//     alert(`An error occured: ${error.message}`);
+// }
+// document.writeln(eval("20+390"))
+
+
+
+
+//////////////////////////// BOM --> Browser Obj Model /////////////////////////
+
+// navigator --> is a object used to display information about browser 
+// location --> is object used to display current url of the web page
+// screen --> is object which is used to display information about the screen
+// window --> 
+
+document.writeln(window.innerHeight+"<br>"+window.innerWidth+"<br>"+ navigator.userAgent)
+
